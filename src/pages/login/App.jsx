ba136f675d9035cd3ddcac412923bed7aeb554e9
beferreira <158208950+beferreira@users.noreply.github.com>
2024-08-31 01:15:41 -0300

    Add files via upload

---
+++ src/pages/login/App.jsx
@@ -0,0 +1,94 @@
+import './App.scss';
+import './global.scss'
+
+function App() {
+
+
+
+    return (
+        <div className="pagina-de-login">
+
+        <div className='espacoUm'>
+
+            <img className='face' src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="" />
+
+            <div className="container-login">
+                    <p>Entrar no Facebook</p>
+
+                    <form className="formulario-login">
+
+                    <input type="text" placeholder="Email ou telefone" className="input-login"/>
+                    <input type="password" placeholder="Senha" className="input-login"/>
+
+                    <button type="submit" className="botao-login"> Entrar </button>
+                    </form>
+                        <a href="#" className="esqueceu-senha">Esqueceu a senha?</a>
+                    
+                    <div className='ouDivisor'>
+                        <div className='line'></div>
+                        <div className='ouTxt'>ou</div>
+                        <div className='line'></div>
+                    </div>
+
+                    <button className="botao-criar-conta">Criar nova conta</button>
+            </div>
+        </div>
+        <footer className="rodape">
+        <nav className='navUm'>
+            <a href="">Portugues (Brasil)</a>
+            <a href="">English (US)</a>
+            <a href="">Espanõl</a>
+            <a href="">Français (France)</a>
+            <a href="">Italiano</a>
+            <a href="">Deutsch</a>
+            <a href="">العربية</a>
+            <a href="">हिन्दी</a>
+            <a href="">中文(简体)</a>
+            <a href="">日本語</a>
+            <button className='plus'></button>
+        </nav>
+        <div className='lineDois'></div>
+        <nav className='navDois'>
+            <a href="">Cadastre-se</a>
+            <a href="">Entrar</a>
+            <a href="">Messenger</a>
+            <a href="">Facebook Lite</a>
+            <a href="">Vídeo</a>
+            <a href="">Locais</a>
+            <a href="">Jogos</a>
+            <a href="">Marketplace</a>
+            <a href="">Meta Pay</a>
+            <a href="">Meta Store</a>
+            <a href="">Meta Quest</a>
+            <a href="">Meta Ray-Ban</a>
+            <a href="">Meta AI</a>
+            <br />
+            <a href="">Instagram</a>
+            <a href="">Threads</a>
+            <a href="">Campanhas de arrecadação de fundos</a>
+            <a href="">Serviços</a>
+            <a href="">Central de Informações de Votação</a>
+            <a href="">Política de Privacidade</a>
+            <a href="">Central de Privacidade</a>
+            <a href="">Grupos</a>
+            <br />
+            <a href="">Sobre</a>
+            <a href="">Criar anúncio</a>
+            <a href="">Criar Página</a>
+            <a href="">Desenvolvedores</a>
+            <a href="">Carreiras</a>
+            <a href="">Cookies</a>
+            <a href="">Escolhas para anúncios</a>
+            <a href="">Termos</a>
+            <a href="">Ajuda</a>
+            <a href="">Carregamento de contatos e não usuários</a>
+            <br />
+          </nav>
+
+          <p className='meta'>Meta © 2024</p>
+        </footer>
+      </div>
+    );
+  }
+  
+  export default App;
